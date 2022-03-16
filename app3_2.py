006803560e764057a1394f2b88e52ded5fb633b1
Luqin <glq.gan@gmail.com>
2022-03-16 14:41:28 -0400

    installed pack

--- app3_2.py
+++ app3_2.py
@@ -5,8 +5,8 @@ import pickle
 import plotly.graph_objects as go
 ### Dash
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
+
 import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input
 ## Navbar
--- home.py
+++ home.py
@@ -1,8 +1,8 @@
 ##### import packages 
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
+
 
 from nav import *
 #from ipynb.fs.full.navbar import Navbar
